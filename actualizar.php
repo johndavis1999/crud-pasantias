616e924ef018c4f59ee95b817e04e5ecf65c0451
johndavis1999 <jdyglol123@gmail.com>
2023-09-11 15:21:24 -0500

    first commit

---
+++ actualizar.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once('conexion.php');
+//actualizar registros
+
+$sql = "UPDATE usuarios SET apellido = 'Britto' WHERE id = 1";
+
+if (
+    $conexion->query($sql) === TRUE
+) {
+    echo "Registro actualizado correctamente";
+} else {
+
+    $conexion->error;
+};
